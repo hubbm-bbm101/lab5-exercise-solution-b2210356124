4b29b3366b81bcbea8767b81cba73e5925f93d4a
b2210356124 <91731736+b2210356124@users.noreply.github.com>
2021-11-05 16:27:40 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,8 +1,14 @@
 while True:
     mail=input("Please enter your e-mail: ")
-    x=mail.count("@")
-    y=mail.count(".")
-    if x==1 and y==1 or x==1 and y>1:
+    x=0 #counter for @
+    y=0 #counter for .
+    for i in range (0,len(mail)):
+        if mail[i]=="@":
+            x+=1
+    for j in range (0,len(mail)):
+        if mail[j]==".":
+            y+=1
+    if x == 1 and y >= 1:
         print("Your e-mail address is correct.")
         break
     else:
